ec159eea0da3d20e8b51bbf148c1bcfea861d02a
Brigibel12 <97652118+Brigibel12@users.noreply.github.com>
2024-02-26 16:57:37 -0400

    Creando script

---
+++ basico-ubuntu.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+echo "Instalando estructura basica para clase virtualhost y proxy reverso"
+
+# Habilitando la memoria de intercambio.
+sudo dd if=/dev/zero of=/swapfile count=2048 bs=1MiB
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+
+# Instando los software necesarios para probar el concepto.
+sudo apt update && sudo apt -y install zip unzip nmap apache2 certbot tree
+
+# Instalando la versión sdkman y java
+curl -s "https://get.sdkman.io" | bash
+source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+# Utilizando la versión de java 17 como base.
+sdk install java 17.0.9-tem
+
+# Subiendo el servicio de Apache.
+sudo service apache2 start
+
+# Clonando el repositorio.
+git clone https://github.com/Brigibel12/practica4-Brigibel.git && practica4-Brigibel
+chmod +x script.sh && bash script.sh
+
+# Copiando los archivos de configuración en la ruta indicada.
+sudo cp ~/practica4-Brigibel/seguro.conf /etc/apache2/sites-available/
+
+# Moviendo a la carpeta app1.
+cd app1
+
+# Ejecutando la creación de fatjar
+chmod +x gradlew
+./gradlew shadowjar
+
+# Subiendo la aplicación puerto por defecto.
+java -jar ~/practica4-Brigibel/app1/build/libs/app.jar > ~/practica4-Brigibel/app1/build/libs/salida.txt 2> ~/practica4-Brigibel/app1/build/libs/error.txt &
+
+# Moviendo a la carpeta app2.
+cd ../app2
+
+# Ejecutando la creación de fatjar
+chmod +x gradlew
+./gradlew shadowjar
+
+# Subiendo la aplicación puerto por defecto.
+java -jar ~/practica4-Brigibel/app2/build/libs/app.jar > ~/practica4-Brigibel/app2/build/libs/salida.txt 2> ~/practica4-Brigibel/app2/build/libs/error.txt &
